841dd88bf9cc8ed47d9d1297edc97520550831ac
Gustavo Jerônimo Moura de França <CIANDT\gjeronimo@lnb029682cps.ciandt.global>
2024-05-25 14:32:20 -0300

    Obtendo usuário pelo ID

--- workshop_spring_boot_mongodb/workshop_spring_mongo/workshopmongo/src/main/java/com/gjmoura/workshopmongo/services/UserService.java
+++ workshop_spring_boot_mongodb/workshop_spring_mongo/workshopmongo/src/main/java/com/gjmoura/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.gjmoura.workshopmongo.services;
 
 import com.gjmoura.workshopmongo.domain.User;
 import com.gjmoura.workshopmongo.repository.UserRepository;
+import com.gjmoura.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,4 +18,14 @@ public class UserService {
     public List<User> findAll() {
         return repo.findAll();
     }
+
+    public User findById(String id) {
+        Optional<User> optionUser = repo.findById(id);
+        if (optionUser.isPresent()) {
+            return optionUser.get();
+        }
+
+        throw new ObjectNotFoundException("Objeto nao encontrado");
+    }
+
 }
