8c8305f88913109214268578c9593037fb44f503
Gustavo Jerônimo Moura de França <CIANDT\gjeronimo@lnb029682cps.ciandt.global>
2024-05-25 18:07:30 -0300

    Cria entity POST com User Aninhado

---
+++ workshop_spring_boot_mongodb/workshop_spring_mongo/workshopmongo/src/main/java/com/gjmoura/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.gjmoura.workshopmongo.repository;
+
+import com.gjmoura.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
