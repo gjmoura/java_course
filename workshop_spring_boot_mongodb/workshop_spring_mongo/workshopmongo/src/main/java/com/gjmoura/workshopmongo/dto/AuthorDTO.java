f280383c0e361b162669a48c25b7d9def0b8cd86
Gustavo Jerônimo Moura de França <CIANDT\gjeronimo@lnb029682cps.ciandt.global>
2024-05-26 22:55:01 -0300

    Endpoint para retornar os posts de um usuário

--- workshop_spring_boot_mongodb/workshop_spring_mongo/workshopmongo/src/main/java/com/gjmoura/workshopmongo/dto/AuthorDTO.java
+++ workshop_spring_boot_mongodb/workshop_spring_mongo/workshopmongo/src/main/java/com/gjmoura/workshopmongo/dto/AuthorDTO.java
@@ -10,11 +10,26 @@ public class AuthorDTO implements Serializable {
     private String name;
 
     public AuthorDTO() {
-
     }
 
     public AuthorDTO(User obj) {
         id = obj.getId();
         name = obj.getName();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
--- workshop_spring_boot_mongodb/workshop_spring_mongo/workshopmongo/src/main/java/com/gjmoura/workshopmongo/resources/UserResource.java
+++ workshop_spring_boot_mongodb/workshop_spring_mongo/workshopmongo/src/main/java/com/gjmoura/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.gjmoura.workshopmongo.resources;
 
+import com.gjmoura.workshopmongo.domain.Post;
 import com.gjmoura.workshopmongo.domain.User;
 import com.gjmoura.workshopmongo.dto.UserDTO;
 import com.gjmoura.workshopmongo.services.UserService;
@@ -58,5 +59,13 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value="/{id}/posts",method = RequestMethod.GET)
+    public ResponseEntity<List<Post>>  findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        List<Post> posts = obj.getPosts();
+
+        return ResponseEntity.ok().body(posts);
     }
 }
