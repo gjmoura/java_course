a01acc9a25002ac98e564f440a8b724472d58655
Gustavo Jerônimo Moura de França <CIANDT\gjeronimo@lnb029682cps.ciandt.global>
2024-05-26 22:34:45 -0300

    Referenciando os posts do usuário

--- workshop_spring_boot_mongodb/workshop_spring_mongo/workshopmongo/src/main/java/com/gjmoura/workshopmongo/domain/Post.java
+++ workshop_spring_boot_mongodb/workshop_spring_mongo/workshopmongo/src/main/java/com/gjmoura/workshopmongo/domain/Post.java
@@ -5,6 +5,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.Date;
 import java.util.Objects;
 
